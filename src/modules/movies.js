0a103d9bda11d112fd17b225b2a89844162ebc30
Stanley Espinoza <stanleysmart12@gmail.com>
2020-02-20 11:14:34 -0600

    Sort and pagin function

--- src/modules/movies.js
+++ src/modules/movies.js
@@ -10,18 +10,39 @@ class Movies extends React.Component {
     };
 
     componentDidMount() {
-        fetch(config.urlAPI + '/api/movies')
+        var p = [];
+        this.getMovies(p);
+    }
+
+    getMovies = (params) => {
+
+        var urlAPI = config.urlAPI + '/api/movies';
+
+        if (params.length > 0)
+        {
+            urlAPI += '?';
+            params.forEach((par) => {
+                urlAPI += par.name + '=' + par.value + '&';
+            });
+        }
+
+        fetch(urlAPI)
         .then(res => res.json()).then((data) => {
             this.setState({movies: data})
         }).catch(console.log);
     }
 
-    query = () => {
-        var q = this.refs.query.value;
-        fetch(config.urlAPI + '/api/movies?q=' + q)
-        .then(res => res.json()).then((data) => {
-            this.setState({movies: data})
-        }).catch(console.log);
+
+    getQueryParam = () => {
+
+        var params = [
+            { name: "q", value: this.refs.query.value },
+            { name: "sort", value: this.refs.sort.value },
+            { name: "p", value: this.refs.page.value },
+            { name: "psize", value: this.refs.pagesize.value }
+        ]
+
+        this.getMovies(params);
     }
 
     render () {
@@ -38,12 +59,31 @@ class Movies extends React.Component {
                         <Col><h1>Movies</h1></Col>
                     </Row>
                     <br/>
+                    <Form>
                     <Row >
                         <Col md={4}>
-                            <Form.Control ref="query" placeholder="Search for movie" onChange={ () => this.query() } />
+                            <label>Search movie:</label>
+                            <Form.Control ref="query" placeholder="Search for movie" onChange={ () => this.getQueryParam() } />
                         </Col>
-                        <Col md={3}></Col>
+                        <Col md={4}>
+                            <label>Sort by:</label>
+                            <select ref="sort" className="form-control" onChange={ () => this.getQueryParam() }>
+                                <option value="">Title</option>
+                                <option value="title_desc">Title Desc</option>
+                                <option value="popularity">Popularity</option>
+                                <option value="popularity_desc">Popularity Desc</option>
+                            </select>
+                        </Col>
+                        <Col md={2}>
+                            <label>Page:</label>
+                            <Form.Control ref="page" onChange={ () => this.getQueryParam() } />
+                        </Col>
+                        <Col md={2}>
+                            <label>Page Size:</label>
+                            <Form.Control ref="pagesize" onChange={ () => this.getQueryParam() } />
+                        </Col>
                     </Row>
+                    </Form>
                     <br/>
                     <Row>
                         {
